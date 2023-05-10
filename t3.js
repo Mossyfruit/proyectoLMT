2f76f56d9ecf17c97952914218ad77b4e2678859
Mossyfruit <116571263+Mossyfruit@users.noreply.github.com>
2023-05-10 13:49:29 +0200

    Initial commit

---
+++ t3.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+
+function boton1(){
+    location.href = "saludos.html";
+    }
+
+function boton2(){
+  alert("Ivan Hijano Soler \n1ºDAW \nPagina guia de un juego con foro/noticias/wiki");
+}
